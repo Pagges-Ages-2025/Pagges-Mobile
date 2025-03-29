68b396673fa0701df499ddd75a4c7be04aadfc14
joao.morandi <jvitormorandi@gmail.com>
2025-03-29 17:09:32 -0300

    Code refactor

--- app/components/Buttons/ActionButton.tsx
+++ app/components/Buttons/ActionButton.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { TouchableOpacity, Image, GestureResponderEvent } from 'react-native';
-import buttonImg from '../../assets/images/EditIcon.png'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function ActionButton(){
+
+export default function ActionButton(props: {color?: string, size: number}){
     function onPress(){
         console.log("Action Button Pressed")
     } 
-
+    
     return(
         <TouchableOpacity onPress={onPress}>
-            <Image source={buttonImg}/>
+           <MaterialCommunityIcons
+                name="square-edit-outline"
+                size={20}
+                color= {props.color ? props.color : "#A9A8A9"}
+              />
         </TouchableOpacity>
     )
 }
