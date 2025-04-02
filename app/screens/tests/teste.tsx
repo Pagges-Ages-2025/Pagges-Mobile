00c4012d150fae1afafa6dadb57e9a8964a6153d
ArthurBlasi <rthrblasi@gmail.com>
2025-04-02 18:22:38 -0300

    clearing teste.tsx

--- app/screens/tests/teste.tsx
+++ app/screens/tests/teste.tsx
@@ -5,12 +5,13 @@ import {
   ScrollView,
   Animated,
   TouchableOpacity,
+  GestureResponderEvent,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTheme } from "@/app/context/ThemeContext";
 import ButtonSection from "./sections/buttonsSection";
-import UserStats from "@/app/components/UserStats/UserStats";
+import CustomButton from "@/app/components/Buttons/CustomButton";
 
 export default function TestsScreen() {
   const fadeAnim = useRef(new Animated.Value(0)).current;
@@ -47,7 +48,10 @@ export default function TestsScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: theme.Background }} edges={["top"]}>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: theme.Background }}
+      edges={["top"]}
+    >
       <ScrollView contentContainerStyle={styles.scroll}>
         {/* Botão de troca de tema */}
         <View style={styles.themeToggleContainer}>
@@ -65,10 +69,11 @@ export default function TestsScreen() {
 
         {/* Seções */}
         <View style={styles.container}>
-        <UserStats kmLidos={100} livros={70} ranking={4} amigos={25} />
+          <ButtonSection />
         </View>
 
-
+        {/* importar aqui o novo componente para poder testar */}
+      
       </ScrollView>
     </SafeAreaView>
   );
