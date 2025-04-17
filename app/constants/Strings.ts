3c8a942fe8b176b7410054fe0bdcd752b40a50fe
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2025-04-17 18:50:51 -0300

    fix: ajustando oq foi solicitado no MR do francisco

--- app/constants/Strings.ts
+++ app/constants/Strings.ts
@@ -4,5 +4,6 @@ export default {
   readLess: "Mostrar menos",
   spoilerWarning: "⚠ Contém spoiler ⚠",
   spoilerWarningSecondary: "Clique para ler",
+  placeholderReview:"Algo a falar sobre seu livro ?"
 };
 
