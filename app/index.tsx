ca8cff29806e223315c09fd22cad94c1d9d2f749
Guilherme Moraes <guimps1909@gmail.com>
2025-05-08 23:45:25 -0300

    fix: application screen entrypoint

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
-import { useRouter } from "expo-router";
-import { View, ActivityIndicator, Text } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import useLoadFonts from "./hooks/useLoadFonts";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
 import NunitoText from "./components/Texts/NunitoText";
+import useLoadFonts from "./hooks/useLoadFonts";
 
 export default function Index() {
   const router = useRouter();
@@ -15,7 +15,7 @@ export default function Index() {
       if (userToken) {
         router.replace("/screens/splash");
       } else {
-        router.replace("/screens/personalLibrary");
+        router.replace("/screens/splash");
       }
     };
 
