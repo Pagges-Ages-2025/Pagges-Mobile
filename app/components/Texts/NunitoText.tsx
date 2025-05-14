7bc5ddf975bfa0d20c192f87a34176194043c227
Bernardo Lorenzi Müller <bernardomuller@MacBook-Air-de-Bernardo-4.local>
2025-05-14 18:31:28 -0300

    kill main

--- app/components/Texts/NunitoText.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Text, TextProps } from "react-native";
-
-export default function NunitoText(props: TextProps) {
-  return <Text {...props} style={[{ fontFamily: "Nunito" }, props.style]} />;
-}
