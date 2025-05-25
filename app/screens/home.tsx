298c1bf8e030a323ecf52d549cdb22e9b6449fc6
Vinicius Wittler <viniwittler@hotmail.com>
2025-05-25 15:58:04 -0300

    ajust home

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -10,6 +10,8 @@ import CustomCarousel from "../components/Carousel/CustomCarousel";
 import CustomBook from "../components/Book/CustomBook";
 import ModalBookDetails from "./bookDetails";
 import BooksService from "../services/booksService";
+import CustomButton from "../components/Buttons/CustomButton";
+import { router } from "expo-router";
 
 export interface Book {
   id: number;
@@ -28,12 +30,6 @@ const mockCards = [
   { id: "2", title: "Desafio Diário" },
   { id: "3", title: "Desafio Diário" },
 ];
-import CustomCarousel from "../components/Carousel/CustomCarousel";
-import CustomButton from "../components/Buttons/CustomButton";
-import { router } from "expo-router";
-
-const Home: React.FC = () => {
-  const { theme } = useTheme();
   const gener = [
     "Terror",
     "Romance",
@@ -49,16 +45,107 @@ const Home: React.FC = () => {
     })}></CustomButton>
   ));
 
+
+const Home: React.FC = () => {
+  const { theme } = useTheme();
+  const { getTrendingBooks } = BooksService();
+  const [trendingBooks, setTradingBooks] = useState<Book[]>();
+  const [selectedTrendingBook, setSelectedTrendingBook] = useState<Book | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    setSelectedTrendingBook(null);
+  };
+
+  const handleSelectTrendingBook = (book: Book) => {
+    setSelectedTrendingBook(book);
+    setModalVisible(true);
+  };
+
+  const fetchTrendingBooks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const results = await getTrendingBooks();
+      const formattedResults = Array.isArray(results) ? results : [];
+      setTradingBooks(formattedResults);
+    } catch (error) {
+      console.error("Erro ao buscar livros em alta:", error);
+      setTradingBooks([]);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchTrendingBooks();
+  }, [fetchTrendingBooks]);
+
   return (
-    <SafeAreaView
-      style={[styles.container, { backgroundColor: theme.Background }]}
-    >
-      <View style={styles.content}>
-        <StaticSearchBar />
+    <SafeAreaView style={[styles.container, { backgroundColor: theme.Background }]}>
+      <ScrollView>
+        <View style={styles.content}>
+          <StaticSearchBar />
+
+          <View style={styles.carouselContainer}>
+            <HomeCarouselSection route={"/screens/home"} cards={mockCards} />
+          </View>
+          
         <View style={styles.genreContent}>
           <CustomCarousel isHorizontal data={genres} />
         </View>
-      </View>
+
+          <NunitoText
+            style={[
+              styles.secondTitle,
+              { paddingBottom: 15, color: theme.primaryText },
+            ]}
+          >
+            Em alta
+          </NunitoText>
+
+          <CustomCarousel
+            isHorizontal
+            data={
+              trendingBooks
+                ? trendingBooks.map((book) => (
+                    <CustomBook
+                      size="small"
+                      key={book.id}
+                      bookId={book.id}
+                      photoPath={book.coverUrl}
+                      title={book.title}
+                      author={book.authors.join(", ")}
+                      onPress={() => handleSelectTrendingBook(book)}
+                    />
+                  ))
+                : []
+            }
+          />
+        </View>
+
+        {selectedTrendingBook && (
+          <ModalBookDetails
+            visible={modalVisible}
+            onClose={handleCloseModal}
+            rating={selectedTrendingBook.avgRating || 1}
+            title={selectedTrendingBook.title || "Título não disponível"}
+            pages={selectedTrendingBook.pages || 0}
+            synopsis={selectedTrendingBook.synopsis || "Sinopse não disponível"}
+            review="Sem avaliações disponíveis ainda."
+            authors={selectedTrendingBook.authors?.join(", ") || "Autor desconhecido"}
+            year={selectedTrendingBook.publicationYear?.substring(0, 4) || "Desconhecido"}
+            id={selectedTrendingBook.id?.toString() || "0"}
+            genre={selectedTrendingBook.genres?.[0] || "Gênero não especificado"}
+            google_image_url={selectedTrendingBook.coverUrl || ""}
+            onCreateReview={() =>
+              console.log("Criar resenha para:", selectedTrendingBook.title)
+            }
+            onShare={() => console.log("Compartilhar:", selectedTrendingBook.title)}
+          />
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -73,8 +160,18 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     width: "90%",
   },
-  genreContent: {
+  carouselContainer: {
+    paddingTop: 20,
+  },
+  genreContent:{
+    paddingTop:30,
+  },
+  secondTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
     paddingTop: 30,
+    paddingLeft: 30,
+    paddingBottom: 10,
   },
 });
 
